00bc3d01055e8c047f58ddcea5490986754433b9
NidhiPatel <patelnidhi2398@gmail.com>
2020-11-06 22:29:32 +0530

    movefile

--- admin_temp/model/signin_model.js
+++ admin_temp/model/signin_model.js
@@ -4,12 +4,12 @@ var item = {
     isauth1:function(item, callback) {
         
         console.log("inside model");
-        return db.query('select * from login where Login_ID = ? and  Password = ? ', [item.username, item.password], callback);
+        return db.query('select * from login where Login_ID = ? and  Password = ? ', [item.email, item.password], callback);
     },
-    // addFlat:function (item,callback) {
-    //     console.log("Dont worry Flat Added");
-    //     return db.query('insert into flat values(?,?,?,?)',[item.,item.,item.,item.],callback);
-    // }
+    addFlat:function (item,callback) {
+        console.log("Dont worry Flat Added");
+        return db.query('insert into flat values(?,?)',[item.flat_no,item.bhk],callback);
+    }
 };
 
 module.exports = item;
