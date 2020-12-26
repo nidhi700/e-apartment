c2d1b6d637361fa69a290c565027b1429842d767
Krima Shah <201912001@daiict.ac.in>
2020-12-26 23:37:12 +0530

    User updated

--- user/model/complaints_model.js
+++
@@ -1,9 +0,0 @@
-var db=require('../dbconnection');
-
-var item = {
-    viewcomplaints:function(callback) {    
-        return db.query("select * from complaints",callback);   
-    }
-};
-
-module.exports = item;
